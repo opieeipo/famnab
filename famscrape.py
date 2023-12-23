eda91fc7904a805783fd36d46c0e7ba15046cb0a
unknown <opie@rebelsam.com>
2023-12-23 16:06:43 +0600

    externalized URL, added argcheck, added support for FAH grab

--- famscrape.py
+++ famscrape.py
@@ -11,12 +11,15 @@ import argparse
 parser=argparse.ArgumentParser()
 
 parser.add_argument("--doc_dir", help="Provide the name of the directory where you want to store the docs")
+parser.add_argument("--famurl", help="Provide the url for the fam")
 
 args=parser.parse_args()
 
 #Set the variables from the command line arguments
 doc_folder_path=args.doc_dir
-if doc_folder_path is None:
+origurl = args.famurl
+#'https://fam.state.gov'
+if doc_folder_path is None or origurl is None:
  parser.print_help()
 else:
   if not os.path.exists(doc_folder_path):
@@ -26,12 +29,6 @@ else:
   op.add_argument('--headless')
   driver = webdriver.Chrome(options=op)
 
-  origurl = 'https://fam.state.gov'
-
-  soupbowl = os.path.join(doc_folder_path,"fam.state.gov.txt")
-  f = open(soupbowl,"w",encoding="utf-8")
-  f.writelines(soupbowl)
-  f.close()
 
   def getQuickSoup(url):
     soup = BeautifulSoup(subprocess.check_output(['curl',url]))
@@ -92,8 +89,43 @@ else:
     for i in ilinks:
       print ("getting Second Level Details for "+i)
       GetSecondLevelDetails(i)
+  
+  def GetFirstLevelFAHDetails(url):
+    print ("Getting First Level Details for "+url)
+    firstLevelSoup=getQuickSoup(url) 
+    f = open(soupbowl,"a",encoding="utf-8")
+    f.writelines(firstLevelSoup.get_text())
+    f.close
+    bodycontent=firstLevelSoup.find_all("div",{"class":"dropdown-menu"})
+    links = bodycontent[1].find_all('a', href=True) 
+    ilinks=[]
+    for b in links:
+      a=b['href']  
+      if "FAH" in a:
+        alink = origurl + a
+        ilinks.append(alink)
+    for i in ilinks:
+      print (i)
+    for i in ilinks:
+      print ("getting Second Level Details for "+i)
+      GetSecondLevelDetails(i)
+
+  #Create FAM.txt File
+  soupbowl = os.path.join(doc_folder_path,"FAM.txt")
+  f = open(soupbowl,"w",encoding="utf-8")
+  f.writelines(soupbowl)
+  f.close()
 
   GetFirstLevelDetails(origurl)
+  
+  #Create FAH.txt file
+  soupbowl = os.path.join(doc_folder_path,"FAH.txt")
+  f = open(soupbowl,"w",encoding="utf-8")
+  f.writelines(soupbowl)
+  f.close()
+
+  GetFirstLevelFAHDetails(origurl)
+  
   driver.close()
   driver.quit()
 
